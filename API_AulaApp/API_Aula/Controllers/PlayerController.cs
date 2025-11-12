a756e74ed7c5c238eff0774e50148530783a80d7
Yam Mike <mikkyamn@gmail.com>
2025-11-11 22:24:18 -0300

    wip

---
+++ API_AulaApp/API_Aula/Controllers/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API_Aula.Controllers
+{
+    public class AlunoController : ControllerBase
+    {
+        public static List<Model.Player> players = new()
+        {
+            new Model.Player { id = 1, Vida = 100, QuantidadeDeItens = 0, PosicaoX = 0, PosicaoY = 0, PosicaoZ = 0}
+        };
+        [HttpGet]
+        [Route("api/player")]
+
+        public IActionResult GetPlayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet]
+        [Route("api/player/{id}")]
+
+        public IActionResult GetPlayer(int id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+
+        [HttpPost]
+        [Route("api/player")]
+
+        public IActionResult AddPlayer([FromBody] Model.Player novoPlayer)
+        {
+            players.Add(novoPlayer);
+            return Ok(novoPlayer);
+        }
+
+        [HttpPut]
+        [Route("api/player/{id}")]
+        public IActionResult UpdatePlayer(int id, [FromBody] Model.Player playerAtualizado)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.Vida = playerAtualizado.Vida;
+            player.QuantidadeDeItens = playerAtualizado.QuantidadeDeItens;
+            player.PosicaoX = playerAtualizado.PosicaoX;
+            player.PosicaoY = playerAtualizado.PosicaoY;
+            player.PosicaoZ = playerAtualizado.PosicaoZ;
+            return Ok(player);
+        }
+
+        [HttpDelete]
+        [Route("api/player/{id}")]
+        public IActionResult DeletePlayer(int id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            players.Remove(player);
+
+            return Ok(player);
+        }
+    }
+}
---
+++ API_AulaApp/API_Aula/Model/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Aula.Model
+{
+    public class Player
+    {
+        public int id { get; set; }
+        public int Vida { get; set; }
+        public int QuantidadeDeItens { get; set; }
+        public int PosicaoX { get; set; }
+        public int PosicaoY { get; set; }
+        public int PosicaoZ { get; set; }
+    }
+}
